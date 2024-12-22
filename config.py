bdbd270f1ae009e069ed4e18be15cb4d4019d5af
INDOFLASH <flashindo@yandex.com>
2024-12-22 15:00:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7690966712:AAEAQ1ZE3iKB9MRW0pbO8JGdKw2lDB0l1Jg")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("TG_FLASH92")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7303380062")
+
+
+
+
+
+
+
+  
